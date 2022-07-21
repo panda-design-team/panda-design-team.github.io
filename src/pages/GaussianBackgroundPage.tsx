a4362e7fad7150682a3a08b6c12e58c232b5f5ed
Cong Zhang <dancerphil1994@gmail.com>
2022-07-21 16:30:19 +0800

    Use published `gaussian-background-generator`

--- src/pages/GaussianBackgroundPage.tsx
+++ src/pages/GaussianBackgroundPage.tsx
@@ -1,56 +1,64 @@
 import styled from '@emotion/styled';
+import {Layer} from 'gaussian-background-generator';
 import {Page, Grid} from '@/components';
-import GaussianBackground from '@/components/GaussianBackground/GaussianBackground';
+import GaussianBackground from '@/components/GaussianBackground';
 
-const colors = [
+const layersBlue: Layer[] = [
     {
-        'color': '#0147fe',
-        'orbs': 8,
-        'radius': 8,
-        'maxVelocity': 0.2,
+        color: '#0147fe',
+        orbs: 8,
+        radius: 8,
+        maxVelocity: 0.2,
     },
     {
-        'color': '#298eff',
-        'orbs': 6,
-        'radius': 6,
-        'maxVelocity': 0.2,
+        color: '#298eff',
+        orbs: 6,
+        radius: 6,
+        maxVelocity: 0.2,
     },
     {
-        'color': '#5de3ff',
+        color: '#5de3ff',
     },
 ];
 
-// 封装为更好用的组件
-const GaussianBackgroundContainer = styled.div`
-    position: relative;
-    border-radius: 5px;
-    overflow: hidden;
-    // 用户侧
+const layersPurple = [
+    {orbs: 4, radius: 20, maxVelocity: 0.2, color: '#3e1969', columns: 2},
+    {orbs: 5, radius: 15, maxVelocity: 0.2, color: '#6f2b9b'},
+    {orbs: 8, radius: 10, maxVelocity: 0.2, color: '#9f3da0'},
+    {color: '#c292e8'},
+];
+
+const layersRed = [
+    {orbs: 4, radius: 20, maxVelocity: 0.2, color: '#710512', columns: 2},
+    {orbs: 5, radius: 15, maxVelocity: 0.2, color: '#b32d45'},
+    {orbs: 8, radius: 10, maxVelocity: 0.2, color: '#ce534b'},
+    {color: '#f4b685'},
+];
+
+const StyledGaussianBackground = styled(GaussianBackground)`
     width: 300px;
     height: 200px;
-    color: white;
     display: flex;
     align-items: center;
     justify-content: center;
-    
-    .gaussian-background {
-        position: absolute;
-        top: 0;
-        right: 0;
-        left: 0;
-        bottom: 0;
-        z-index: -1;
-    }
 `;
 
 export const GaussianBackgroundPage = () => {
     return (
         <Page title="Gaussian Background 呼吸背景" status="开发中">
-            <Grid title="基础样式" repeat={1}>
-                <GaussianBackgroundContainer>
-                    <GaussianBackground colors={colors} blurRadius={20} width="100%" height="100%" />
+            <Grid title="基础样式" repeat={2}>
+                <StyledGaussianBackground>
                     静待发布
-                </GaussianBackgroundContainer>
+                </StyledGaussianBackground>
+                <StyledGaussianBackground layers={layersBlue}>
+                    静待发布
+                </StyledGaussianBackground>
+                <StyledGaussianBackground layers={layersRed}>
+                    静待发布
+                </StyledGaussianBackground>
+                <StyledGaussianBackground layers={layersPurple}>
+                    静待发布
+                </StyledGaussianBackground>
             </Grid>
         </Page>
     );
