9b5fbf950b3475275ab70cf2b75a501ef8e0d2b0
Cong Zhang <dancerphil1994@gmail.com>
2023-03-28 15:43:32 +0800

    Update animejs demo

--- src/pages/AnimationPage.tsx
+++ src/pages/AnimationPage.tsx
@@ -1,13 +1,11 @@
 import {Page} from '@/components/Page';
 import {AnimeGrids} from '@/pages/Grids/AnimeGrids';
 import {GaussianBackgroundGrids} from '@/pages/Grids/GaussianBackgroundGrids';
-import {useRoleType} from '@/regions';
 
 export const AnimationPage = () => {
-    const roleType = useRoleType();
     return (
         <Page title="8. Animation 动画效果">
-            {roleType === '组件库开发' && <AnimeGrids />}
+            <AnimeGrids />
             <GaussianBackgroundGrids />
         </Page>
     );
--- src/pages/Grids/AnimeGrids.tsx
+++ src/pages/Grids/AnimeGrids.tsx
@@ -3,37 +3,70 @@ import {useEffect, useRef} from 'react';
 import styled from '@emotion/styled';
 import {Typography} from 'antd';
 import {css} from '@emotion/css';
+import {colors} from '@panda-design/components';
 
 const Item = styled.div`
-    background-color: #000;
-    opacity: 0.1;
+    background-color: ${colors['success-6']};
 `;
 
+const generateGridItems = (className: string) => {
+    return (
+        <div className={css`display: grid; grid-template-columns: repeat(5, auto); width: 100px; height: 100px;`}>
+            {Array.from({length: 25}).map((_, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <Item key={index} className={className} />
+            ))}
+        </div>
+    );
+};
+
 export const AnimeGrids = () => {
-    const ref = useRef<boolean>();
+    const settledRef = useRef<boolean>();
     useEffect(
         () => {
-            if (!ref.current) {
+            if (!settledRef.current) {
                 anime({
                     targets: '.element-demo-1',
-                    opacity: [0.1, 0.2, 0.1],
-                    delay: anime.stagger(100, {grid: [5, 5], axis: 'y'}),
+                    scale: [
+                        {value: 0.1, easing: 'easeOutSine', duration: 500},
+                        {value: 1, easing: 'easeInOutQuad', duration: 1200},
+                    ],
+                    delay: anime.stagger(200, {grid: [5, 5], axis: 'y'}),
                     easing: 'linear',
                     loop: true,
                 });
-                ref.current = true;
+                anime({
+                    targets: '.element-demo-2',
+                    scale: [
+                        {value: 0.1, easing: 'easeOutSine', duration: 500},
+                        {value: 1, easing: 'easeInOutQuad', duration: 1200},
+                    ],
+                    delay: anime.stagger(200, {grid: [5, 5], from: 'first'}),
+                    easing: 'linear',
+                    loop: true,
+                });
+                anime({
+                    targets: '.element-demo-3',
+                    scale: [
+                        {value: 0.1, easing: 'easeOutSine', duration: 500},
+                        {value: 1, easing: 'easeInOutQuad', duration: 1200},
+                    ],
+                    delay: anime.stagger(200, {grid: [5, 5], from: 'center'}),
+                    easing: 'linear',
+                    loop: true,
+                });
+                settledRef.current = true;
             }
         },
         []
     );
     return (
         <>
-            <Typography.Title>Anime</Typography.Title>
-            <div className={css`display: grid; grid-template-columns: repeat(5, auto); width: 100px; height: 100px;`}>
-                {Array.from({length: 25}).map((_, index) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <Item key={index} className="element-demo-1" />
-                ))}
+            <Typography.Title>AnimeJS (Unstable)</Typography.Title>
+            <div className={css`display: flex; gap: 20px;`}>
+                {generateGridItems('element-demo-1"')}
+                {generateGridItems('element-demo-2"')}
+                {generateGridItems('element-demo-3"')}
             </div>
         </>
     );
