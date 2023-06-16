59f3b36e5eccd060488792f247ab2dfa463bcda7
Cong Zhang <dancerphil1994@gmail.com>
2023-06-16 11:36:40 +0800

    `antd@5.6.1` & `@panda-design/components@0.0.56`

--- src/pages/AnimationPage.tsx
+++ src/pages/AnimationPage.tsx
@@ -1,11 +1,11 @@
 import {Page} from '@/components/Page';
-import {AnimeGrids} from '@/pages/Grids/AnimeGrids';
+// import {AnimeGrids} from '@/pages/Grids/AnimeGrids';
 import {GaussianBackgroundGrids} from '@/pages/Grids/GaussianBackgroundGrids';
 
 export const AnimationPage = () => {
     return (
         <Page title="8. Animation 动画效果">
-            <AnimeGrids />
+            {/* <AnimeGrids /> */}
             <GaussianBackgroundGrids />
         </Page>
     );
