132271aa5d1113b674872f5caa0003948b071823
Cong Zhang <dancerphil1994@gmail.com>
2023-08-28 12:35:53 +0800

    `antd@5.8.4` & `@panda-design/components@0.0.60`

--- src/pages/Grids/GaussianBackgroundGrids.tsx
+++ src/pages/Grids/GaussianBackgroundGrids.tsx
@@ -1,76 +1,135 @@
-import styled from '@emotion/styled';
-import {GaussianBackground, GaussianBackgroundLayer} from '@panda-design/extra';
+import {
+    GaussianBackground,
+    GaussianBackgroundProps,
+    GaussianBackgroundCircleConfig,
+    colors,
+} from '@panda-design/components';
 import {Typography} from 'antd';
+import {css, cx} from '@emotion/css';
 import {Grid} from '@/components/Grid';
 
-const layersLightColorful: GaussianBackgroundLayer[] = [
-    {orbs: 4, radius: 20, maxVelocity: 0.2, color: '#fff'},
-    {orbs: 8, radius: 10, maxVelocity: 0.5, color: '#0ff'},
-    {orbs: 8, radius: 10, maxVelocity: 0.5, color: '#f0f'},
-    {orbs: 8, radius: 10, maxVelocity: 0.5, color: '#ff0'},
-    // {color: '#000'},
+const circlesLightColorful: GaussianBackgroundCircleConfig[] = [
+    {radius: 6, color: '#0ff'},
+    {radius: 6, color: '#0ff'},
+    {radius: 6, color: '#0ff'},
+    {radius: 6, color: '#0ff'},
+    {radius: 3, color: '#f0f'},
+    {radius: 3, color: '#f0f'},
+    {radius: 3, color: '#f0f'},
+    {radius: 3, color: '#f0f'},
+    {radius: 2, color: '#ff0'},
+    {radius: 2, color: '#ff0'},
+    {radius: 2, color: '#ff0'},
+    {radius: 2, color: '#ff0'},
 ];
 
-const layersDarkColorful: GaussianBackgroundLayer[] = [
-    {color: '#2f54eb', orbs: 4, radius: 16, maxVelocity: 0.2},
-    {color: '#1890ff', orbs: 4, radius: 8, maxVelocity: 0.2},
-    {color: '#fadb14'},
+const circlesDarkColorful: GaussianBackgroundCircleConfig[] = [
+    {color: '#1890ff', radius: 6},
+    {color: '#1890ff', radius: 6},
+    {color: '#1890ff', radius: 6},
+    {color: '#1890ff', radius: 6},
+    {color: '#fadb14', radius: 3},
+    {color: '#fadb14', radius: 3},
+    {color: '#fadb14', radius: 3},
+    {color: '#fadb14', radius: 3},
 ];
 
-const layersBlue: GaussianBackgroundLayer[] = [
-    {orbs: 4, radius: 20, maxVelocity: 0.2, color: '#245ca7', columns: 2},
-    {orbs: 5, radius: 15, maxVelocity: 0.2, color: '#2176c7'},
-    {orbs: 7, radius: 12, maxVelocity: 0.2, color: '#149dee'},
-    {color: '#b6d7fd'},
+const circlesBlue: GaussianBackgroundCircleConfig[] = [
+    {color: '#2176c7', radius: 5},
+    {color: '#149dee', radius: 5},
+    {color: '#2176c7', radius: 5},
+    {color: '#149dee', radius: 5},
+    {color: '#2176c7', radius: 5},
+    {color: '#149dee', radius: 5},
+    {color: '#2176c7', radius: 5},
+    {color: '#149dee', radius: 5},
+    {color: '#b6d7fd', radius: 3},
+    {color: '#b6d7fd', radius: 3},
+    {color: '#b6d7fd', radius: 3},
+    {color: '#b6d7fd', radius: 3},
 ];
 
-const layersPurple = [
-    {orbs: 4, radius: 20, maxVelocity: 0.2, color: '#3e1969', columns: 2},
-    {orbs: 5, radius: 15, maxVelocity: 0.2, color: '#6f2b9b'},
-    {orbs: 8, radius: 10, maxVelocity: 0.2, color: '#9f3da0'},
-    {color: '#c292e8'},
+const circlesRed: GaussianBackgroundCircleConfig[] = [
+    {color: '#b32d45', radius: 5},
+    {color: '#ce534b', radius: 5},
+    {color: '#b32d45', radius: 5},
+    {color: '#ce534b', radius: 5},
+    {color: '#b32d45', radius: 5},
+    {color: '#ce534b', radius: 5},
+    {color: '#b32d45', radius: 5},
+    {color: '#ce534b', radius: 5},
+    {color: '#f4b685', radius: 3},
+    {color: '#f4b685', radius: 3},
+    {color: '#f4b685', radius: 3},
+    {color: '#f4b685', radius: 3},
 ];
 
-const layersRed = [
-    {orbs: 4, radius: 20, maxVelocity: 0.2, color: '#710512', columns: 2},
-    {orbs: 5, radius: 15, maxVelocity: 0.2, color: '#b32d45'},
-    {orbs: 8, radius: 10, maxVelocity: 0.2, color: '#ce534b'},
-    {color: '#f4b685'},
+const layersPurple: GaussianBackgroundCircleConfig[] = [
+    {color: '#6f2b9b', radius: 5},
+    {color: '#9f3da0', radius: 5},
+    {color: '#6f2b9b', radius: 5},
+    {color: '#9f3da0', radius: 5},
+    {color: '#6f2b9b', radius: 5},
+    {color: '#9f3da0', radius: 5},
+    {color: '#6f2b9b', radius: 5},
+    {color: '#9f3da0', radius: 5},
+    {color: '#c292e8', radius: 3},
+    {color: '#c292e8', radius: 3},
+    {color: '#c292e8', radius: 3},
+    {color: '#c292e8', radius: 3},
 ];
 
-const StyledGaussianBackground = styled(GaussianBackground)<{ghost?: boolean}>`
-    width: 300px;
+const cssGaussianBackground = css`
+    width: 300px !important;
     height: 200px;
     display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 4px;
-    ${props => props.ghost && 'color: white'};
+    color: white;
 `;
+
+const StyledGaussianBackground = (props: GaussianBackgroundProps) => {
+    return (
+        <GaussianBackground
+            {...props}
+            className={cx(cssGaussianBackground, props.className)}
+            width={300}
+            height={200}
+            blur={15}
+        >
+            描述文字
+        </GaussianBackground>
+    );
+};
 
 export const GaussianBackgroundGrids = () => {
     return (
         <>
             <Typography.Title>Gaussian Background 呼吸背景 (Unstable)</Typography.Title>
             <Grid repeat={3}>
-                <StyledGaussianBackground>
-                    描述文字
-                </StyledGaussianBackground>
-                <StyledGaussianBackground layers={layersLightColorful}>
-                    描述文字
-                </StyledGaussianBackground>
-                <StyledGaussianBackground ghost layers={layersDarkColorful}>
-                    描述文字
-                </StyledGaussianBackground>
-                <StyledGaussianBackground ghost layers={layersBlue}>
-                    描述文字
-                </StyledGaussianBackground>
-                <StyledGaussianBackground ghost layers={layersRed}>
-                    描述文字
-                </StyledGaussianBackground>
-                <StyledGaussianBackground ghost layers={layersPurple}>
-                    描述文字
-                </StyledGaussianBackground>
+                <StyledGaussianBackground style={{color: colors.black}} />
+                <StyledGaussianBackground
+                    backgroundColor="#fff"
+                    style={{color: colors.black}}
+                    circles={circlesLightColorful}
+                />
+                <StyledGaussianBackground
+                    backgroundColor="#2f54eb"
+                    circles={circlesDarkColorful}
+                />
+                <StyledGaussianBackground
+                    backgroundColor="#245ca7"
+                    circles={circlesBlue}
+                />
+                <StyledGaussianBackground
+                    backgroundColor="#710512"
+                    circles={circlesRed}
+                />
+                <StyledGaussianBackground
+                    backgroundColor="#3e1969"
+                    circles={layersPurple}
+                />
             </Grid>
         </>
     );
