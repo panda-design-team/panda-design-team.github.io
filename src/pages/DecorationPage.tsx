cb372a0555815a377b5478c4121dc56e0fa4bd2d
Cong Zhang <dancerphil1994@gmail.com>
2023-09-08 11:27:21 +0800

    `antd@5.8.6` & `@panda-design/components@0.0.63`

--- src/pages/DecorationPage.tsx
+++ src/pages/DecorationPage.tsx
@@ -2,8 +2,11 @@ import {Page} from '@/components/Page';
 import {TagGrids} from '@/pages/Grids/TagGrids';
 import {AlertGrids} from '@/pages/Grids/AlertGrids';
 import {MessageGrids} from '@/pages/Grids/MessageGrids';
+import {MessageDebugGrids} from '@/pages/Grids/MessageDebugGrids';
+import {useRoleType} from '@/regions';
 
 export const DecorationPage = () => {
+    const roleType = useRoleType();
     return (
         <Page title="7. Decoration 装饰">
             <p>比如，我们使用功能色标记「成功」、「失败」、「运行中」等信息，用户在不需要阅读文字的前提下，就可以获知重要信息。</p>
@@ -11,6 +14,7 @@ export const DecorationPage = () => {
             <TagGrids />
             <AlertGrids />
             <MessageGrids />
+            {roleType === '组件库开发' && <MessageDebugGrids />}
         </Page>
     );
 };
--- src/pages/Grids/MessageDebugGrids.tsx
+++ src/pages/Grids/MessageDebugGrids.tsx
@@ -1,18 +1,17 @@
-import {Button, message} from '@panda-design/components';
-import {Typography} from 'antd';
+import {Button} from '@panda-design/components';
+import {Typography, message} from 'antd';
 import {Grid} from '@/components/Grid';
 import {Link} from '@/components/Link';
 
-export const MessageGrids = () => {
+export const MessageDebugGrids = () => {
     return (
         <>
-            <Typography.Title>Message 全局提示</Typography.Title>
-            <Grid repeat={5}>
+            <Typography.Title>附：测试 antd 默认样式</Typography.Title>
+            <Grid repeat={4}>
                 <Button onClick={() => message.info('你好')}>提示</Button>
                 <Button onClick={() => message.success('操作成功')}>成功</Button>
                 <Button onClick={() => message.error(<>操作失败，点击<Link to="https://www.example.com">查看解决方案</Link></>)}>失败</Button>
                 <Button onClick={() => message.warning('警告')}>警告</Button>
-                <Button onClick={() => message.warning({content: '警告', showCloseIcon: false})}>不含关闭按钮</Button>
             </Grid>
         </>
     );
